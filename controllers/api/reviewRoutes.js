82340ddf50411d63611035fd7fa8ee4262802172
Kerby00 <119148777+Kerby00@users.noreply.github.com>
2023-03-22 19:19:53 -0600

    fixed route

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -5,7 +5,10 @@ const withAuth = require('../../utils/auth');
 
 router.post('/', withAuth, async (req, res) => {
     try {
-        const newReview = await Review.create(req.body);
+        const newReview = await Review.create({
+            ...req.body,
+            user_id: req.session.user_id
+        });
 
         res.status(200).json(newReview)
     } catch (err) {
