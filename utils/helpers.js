4ef5121b95220f1b97c48984bda6c2f93cfa2261
Jsheen98 <jsheendev@gmail.com>
2023-03-22 19:07:10 -0600

    Added header and some rendering

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,8 +1,7 @@
 // Helper function for formatting dates on reviews/comments
 module.exports = {
     format_date: (date) => {
-        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${
-          new Date(date).getFullYear()
-        }`;
-      }
+        return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(date).getFullYear()
+            }`;
+    }
 }
