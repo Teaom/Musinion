00428e2b71378d8ec542e078a6a765082732d8a8
BradenKim22 <118400033+BradenKim22@users.noreply.github.com>
2023-03-23 18:45:05 -0600

    create-review and public review

--- config/connection.js
+++ config/connection.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 let sequelize;
+
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
@@ -16,13 +17,5 @@ if (process.env.JAWSDB_URL) {
     }
   );
 }
-module.exports = sequelize;
-
-
-
-
-
-
-
 
 module.exports = sequelize;
--- public/js/review.js
+++ public/js/review.js
@@ -1,1 +1,35 @@
 // Braden
+const createReview = async (event) => {
+    event.preventDefault();
+    
+    // artist_name
+    const artist_name = document.querySelector('#artist').value.trim();
+    // album_name
+    const album_name = document.querySelector('#album').value.trim();
+    // review
+    const review = document.querySelector('#review').value.trim();
+    // review_rating (1-5) .5 are good
+    const review_rating = document.querySelector('#rating').value.trim();
+    // cover_url
+    const cover_url = document.querySelector('#cover').value.trim();
+  
+    if (artist_name && album_name && review && review_rating && cover_url) {
+      const response = await fetch(`/api/review`, {
+        method: 'POST',
+        body: JSON.stringify({ artist_name, album_name, review, review_rating, cover_url }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Failed to create review');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.create-review-form')
+    .addEventListener('submit', createReview);
