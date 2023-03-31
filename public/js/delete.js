13e357f7416d5c241aa0400b41324103802c5cef
Jsheen98 <jsheendev@gmail.com>
2023-03-31 14:24:40 -0600

    Delete for reviews

--- public/js/delete.js
+++ public/js/delete.js
@@ -1,23 +1,23 @@
-// const url = window.location.pathname
-// const filterNumber = url.match(/(\d+)/)
-// const id = filterNumber[1]
+const url = window.location.pathname
+const filterNumber = url.match(/(\d+)/)
+const id = filterNumber[1]
 
-// // Frontend request to delete a blog post
-// const deleteHandler = async (event) => {
-//     event.preventDefault()
+// Frontend request to delete a blog post
+const deleteHandler = async (event) => {
+    event.preventDefault()
 
-//     const response = await fetch(`/api/review/${id}`, {
-//         method: 'DELETE'
-//     })
+    const response = await fetch(`/api/review/${id}`, {
+        method: 'DELETE'
+    })
 
-//     if (response.ok) {
-//         document.location.replace('/profile')
-//     } else {
-//         alert('Blog post could not be deleted')
-//     }
+    if (response.ok) {
+        document.location.replace('/profile')
+    } else {
+        alert('Blog post could not be deleted')
+    }
     
-// }
+}
 
-// document
-//     .querySelector('#delete-review-button')
-//     .addEventListener('click', deleteHandler)
+document
+    .querySelector('#delete-review-button')
+    .addEventListener('click', deleteHandler)
